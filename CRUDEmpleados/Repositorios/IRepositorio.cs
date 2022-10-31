e6faebfa66b090d35ba07a72d09d57df4597958d
Agustin Barberis <agus_101@live.com.ar>
2022-10-31 20:23:55 -0300

    Crea interfaz repositorio

--- CRUDEmpleados/Repositorios/IRepositorio.cs
+++ CRUDEmpleados/Repositorios/IRepositorio.cs
@@ -2,9 +2,10 @@
 {
     public interface IRepositorio<TObjeto,TId >
     {
-
-
-
-
+        TObjeto ObtenerTodos();
+        TObjeto Obtener(TId id);
+        void Agregar(TObjeto objeto);
+        void Eliminar(TId id);
+        void Modificar(TId id, TObjeto objeto);
     }
 }
